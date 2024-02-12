957e11d60506c8d3b350eb87ccaf8e38640ea351
Prathik-2002 <acharyaprathik4@gmail.com>
2024-02-12 19:33:29 +0530

    check

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,13 +1,15 @@
 const {expect} = require('chai');
 
 function batteryIsOk(temperature, soc, charge_rate) {
-    if (temperature < 0 || temperature > 45) {
+    if (!(temperature >= 0 && temperature <=45)) {
         console.log('Temperature is out of range!');
         return false;
-    } else if (soc < 20 || soc > 80) {
+    }
+    if (!(soc >= 20 && soc <= 40)) {
         console.log('State of Charge is out of range!')
         return false;
-    } else if (charge_rate > 0.8) {
+    }
+    if (charge_rate > 0.8) {
         console.log('Charge rate is out of range!');
         return false;
     }
