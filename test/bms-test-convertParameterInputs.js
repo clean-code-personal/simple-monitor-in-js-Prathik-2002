2e43d24ba96d743cafbe7e14822a8a9fa8948237
Prathik-2002 <acharyaprathik4@gmail.com>
2024-02-19 09:12:34 +0530

    added test cases

--- test/bms-test-convertParameterInputs.js
+++ test/bms-test-convertParameterInputs.js
@@ -3,11 +3,14 @@ const {convertToCommonUnit} = require('../bms-convertParameterInputs')
 
 const conversionTestCases = [
     ["temperature", "20 C", 20],
+    ["temperature", "20 K", (-253.15)],
+    ["soc", "20", 20],
+    ["chargeRate", "0.8", (0.8)],
 ]
 
 function ConversionTestDesign(parameterName, parameterValue, expected){
     it(`should return ${expected} for ${parameterName} - ${parameterValue}`,()=>{
-        assert.equal(convertToCommonUnit(parameterName, parameterValue), expected)
+        assert.equal(convertToCommonUnit(parameterName, parameterValue), expected.toFixed(2))
     })
 }
 
